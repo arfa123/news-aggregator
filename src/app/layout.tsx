4e7e0b46beeb38998b384bcecd1d4b7ad369d0d0
arfa123 <arfa.ahmed.feroz@gmail.com>
2024-09-28 18:31:18 +0500

    refactor(next.js): enable partial prerendering
    - Enabled partial prerendering for Next.js 15 to improve performance.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import { ArticleProvider } from "@/contexts/ArticleContext";
 
 import "./globals.css";
 
+export const experimental_ppr = true;
+
 export const metadata: Metadata = {
   title: {
     default: "News Aggregator",
